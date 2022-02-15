ac656d73e16916d01c13404a533c14999b9dbe20
DEII <DEII@DESKTOP-JUVEIQ9>
2022-02-15 13:59:39 -0800

    Add project files.

---
+++ FYMKWebApplication4/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FYMKWebApplication4.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ FYMKWebApplication4/Data/FYMKWebApplication4Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FYMKWebApplication4.Data
+{
+    public class FYMKWebApplication4Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public FYMKWebApplication4Context() : base("name=FYMKWebApplication4Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<FYMKWebApplication4.Models.Mentee> Mentees { get; set; }
+
+        public System.Data.Entity.DbSet<FYMKWebApplication4.Models.Mentor> Mentors { get; set; }
+    }
+}
